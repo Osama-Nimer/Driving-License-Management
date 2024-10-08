2c322e2bf87618a9b623cb68507c604a1b527673
Osama-Nimer <osamafares23@yahoo.com>
2024-10-08 22:35:59 +0300

    connected

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         clsPepoleBuisness clsPepole = clsPepoleBuisness.Find(1030);
         Console.WriteLine(clsPepole.FirstName);
     }
