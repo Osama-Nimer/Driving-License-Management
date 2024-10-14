90be25ba073cd79c7f7ace8e882f2c2eed9dd339
Osama-Nimer <osamafares23@yahoo.com>
2024-10-14 16:08:45 +0300

    ..

--- BuisnessLayer/obj/Debug/net8.0/BuisnessLayer.AssemblyInfo.cs
+++ BuisnessLayer/obj/Debug/net8.0/BuisnessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuisnessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59cc12bdd895bbae182cf897d1d100219815d10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e02be8b579de909e741ff3fee6e05360e4561b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuisnessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuisnessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
