fe53aad30e02fcfbe26f4eeeda7c04a569f61de1
Osama-Nimer <osamafares23@yahoo.com>
2024-10-20 14:41:08 +0300

    User Card Added

---
+++ Driving-License-Management/Users/ctrlUserCard.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace Driving_License_Management.Users
+{
+    partial class ctrlUserCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // ctrlUserCard
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Name = "ctrlUserCard";
+            Size = new System.Drawing.Size(1042, 439);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
